620fa4e7327f4bd25c5a0297d06f3fdfa7ef92e1
HectorRG28 <hectorrecagarcia@gmail.com>
2025-11-14 12:22:29 +0100

    arrego verbos REST

--- controllers/clientesController.js
+++ controllers/clientesController.js
@@ -7,10 +7,13 @@ exports.obtenerTodos = (req, res) => {
 //Obtener por ID
 exports.obtenerPorId = (req, res) => {
     const cliente = clientesService.buscarPorId(parseInt(req.params.id));
-    cliente ? res.json(clientes) : res.status(404).json({mensaje: 'No encontrado'});
+    // Solución: Devolver el "cliente" individual encontrado
+    cliente ? res.json(cliente) : res.status(404).json({mensaje: 'No encontrado'});
 }
+
 //Crear
 exports.crear = (req, res) => {
     const nuevo = clientesService.crear(req.body);
-    res.status()
+    // Solución: Enviar el status 201 y el nuevo cliente creado
+    res.status(201).json(nuevo);
 }
